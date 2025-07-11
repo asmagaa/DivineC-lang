fbfff2d3fd65d3906ea79bfc428511f6fffc39bc
Aleksander Smaga <geraldciekniedripem@gmail.com>
2025-07-11 17:43:22 +0100

    fixed ast definitions

--- frontend/ast.ts
+++ frontend/ast.ts
@@ -1,5 +1,16 @@
 // ast definitions
-export type NodeType = "Program" | "NumericLiteral" | "Identifier" | "BinaryExpr";
+export type NodeType = 
+    | "Program"
+    | "VarDeclaration"
+    | "FunctionDeclaration"
+    | "AssignmentExpr"
+    | "MemberExpr"
+    | "CallExpr"
+    | "Property"
+    | "ObjectLiteral"
+    | "NumericLiteral"
+    | "Identifier"
+    | "BinaryExpr";
 
 export interface Stmt {
     kind: NodeType;
@@ -10,13 +21,46 @@ export interface Program extends Stmt {
     body: Stmt[];
 }
 
+export interface VarDeclaration extends Stmt {
+    kind: "VarDeclaration";
+    constant: boolean;
+    identifier: string;
+    value?: Expr;
+}
+
+export interface FunctionDeclaration extends Stmt {
+    kind: "FunctionDeclaration";
+    parameters: string[];
+    name: string;
+    body: Stmt[];
+}
+
 export interface Expr extends Stmt {}
+
+export interface AssignmentExpr extends Expr {
+    kind: "AssignmentExpr";
+    assigne: Expr;
+    value: Expr;
+}
 
 export interface BinaryExpr extends Expr {
     kind: "BinaryExpr";
     left: Expr;
     right: Expr;
     operator: string;
+}
+
+export interface CallExpr extends Expr {
+    kind: "CallExpr";
+    args: Expr[];
+    caller: Expr;
+}
+
+export interface MemberExpr extends Expr {
+    kind: "MemberExpr";
+    object: Expr;
+    property: Expr;
+    computed: boolean;
 }
 
 export interface Identifier extends Expr {
@@ -27,4 +71,15 @@ export interface Identifier extends Expr {
 export interface NumericLiteral extends Expr {
     kind: "NumericLiteral";
     value: number;
+}
+
+export interface Property extends Expr {
+    kind: "Property";
+    key: string;
+    value?: Expr;
+}
+
+export interface ObjectLiteral extends Expr {
+    kind: "ObjectLiteral";
+    properties: Property[];
 }
